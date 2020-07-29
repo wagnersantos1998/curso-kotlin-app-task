a43903aac9bcfa6b656864584d22f37451689b27
Wagner Santos <Wagner_Santos@live.com>
2020-07-29 18:57:56 -0300

    Melhorando os layouts e finalizando o projeto

--- app/src/main/java/com/example/tasks/view/MainActivity.kt
+++ app/src/main/java/com/example/tasks/view/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.graphics.Color
 import android.os.Bundle
-import android.util.Log.i
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
